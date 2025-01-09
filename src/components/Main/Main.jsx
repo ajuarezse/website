7c2c169973eb2a41b2cb07f5bcfaa02c95c369d8
ajuarezse <ajuarez.se@gmail.com>
2025-01-09 12:13:23 -0800

    install and impliment typewriter effect

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,27 @@
 import "./Main.css";
+import { Typewriter } from "react-simple-typewriter";
 
 function Main() {
   return (
     <main className="main">
       <h2 className="main__title">
         Hi, I'm <span className="main__title-name">Angel</span>
+        <p className="main__typing">
+          I'm a
+          <Typewriter
+            words={[
+              " Front-End Developer",
+              " Back-End Developer",
+              " Full-Stack Web Engineer",
+            ]}
+            loop={Infinity}
+            cursor
+            cursorStyle="_"
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={2000}
+          />
+        </p>
       </h2>
     </main>
   );
