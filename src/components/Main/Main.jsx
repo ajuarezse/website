b1f0e27b95d6c63c6f60d5c5349400c693ac1882
ajuarezse <ajuarez.se@gmail.com>
2025-01-12 15:57:18 -0800

    center and fade in

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -25,6 +25,21 @@ function Main() {
           />
         </span>
       </p>
+      <section className="main__content">
+        <p className="main__about">
+          I’m a developer with a passion for crafting accessible, pixel-perfect
+          user interfaces that seamlessly merge thoughtful design with robust
+          engineering. My work focuses on the intersection of design and
+          development, creating experiences that are visually stunning,
+          high-performing, and user-friendly. As a Junior Full-Stack Engineer, I
+          specialize in building and maintaining UI components that power
+          front-end platforms while ensuring they meet web accessibility
+          standards and best practices to deliver inclusive user experiences. In
+          my career, I’ve had the privilege of developing software across
+          diverse environments, gaining valuable insights into building scalable
+          and efficient solutions.
+        </p>
+      </section>
     </main>
   );
 }
