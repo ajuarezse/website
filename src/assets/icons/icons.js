f6233cbf7439c80e3956ef992c9cf4d7495e58f6
ajuarezse <ajuarez.se@gmail.com>
2025-01-16 23:29:31 -0800

    add icon batch

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,4 +1,21 @@
 import { ReactComponent as GithubIcon } from "./github-svgrepo-com.svg";
 import { ReactComponent as WebIcon } from "./website-dashboard-svgrepo-com.svg";
+import { ReactComponent as GcpIcon } from "./brand-google-cloud-platform-svgrepo-com.svg";
+import { ReactComponent as CssIcon } from "./css3-01-svgrepo-com.svg";
+import { ReactComponent as HtmlIcon } from "./html-fill-svgrepo-com.svg";
+import { ReactComponent as JavascriptIcon } from "./javascript-16-svgrepo-com.svg";
+import { ReactComponent as LinkedinIcon } from "./linkedin-outline-svgrepo-com.svg";
+import { ReactComponent as NodejsIcon } from "./nodejs01-svgrepo-com.svg";
+import { ReactComponent as ReactIcon } from "./react-logo-programming-svgrepo-com.svg";
 
-export { GithubIcon, WebIcon };
+export {
+  GithubIcon,
+  WebIcon,
+  GcpIcon,
+  CssIcon,
+  HtmlIcon,
+  JavascriptIcon,
+  LinkedinIcon,
+  NodejsIcon,
+  ReactIcon,
+};
