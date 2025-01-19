85e1818663c32aafabe86af954ea059c42cbd8be
ajuarezse <ajuarez.se@gmail.com>
2025-01-19 15:13:22 -0800

    add header tuck away with scroll down effect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [scrollDirection, setScrollDirection] = useState("up");
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 50) {
+        setScrollDirection("down");
+      } else if (currentScrollY < lastScrollY) {
+        setScrollDirection("up");
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [lastScrollY]);
+
   return (
-    <header className="header">
+    <header className={`header ${scrollDirection === "down" ? "hide" : ""}`}>
       <a href="#home" className="header__logo">
         Angel <span className="header__logo-last">Juarez</span>
       </a>
