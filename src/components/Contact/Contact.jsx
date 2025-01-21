18bb483b87cde866f286a64508907ddfd23b7be2
ajuarezse <ajuarez.se@gmail.com>
2025-01-20 22:10:12 -0800

    structure basic html css and route

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,6 +4,11 @@ function Contact() {
   return (
     <section className="contact" id="contact">
       <h1 className="contact__header">What's Next?</h1>
+      <h2 className="contact__sub-header">Get in Touch</h2>
+      <p className="contact__text">
+        I’m open to new and exciting opportunities and collaborations! Feel free
+        to reach out and I’ll try my best to get back to you!
+      </p>
     </section>
   );
 }
