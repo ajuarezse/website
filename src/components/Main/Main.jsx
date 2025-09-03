98483de7e4fa232a40b699ba0e7d373bb7402e9a
ajuarezse <ajuarez.se@gmail.com>
2025-09-02 20:24:33 -0700

    Add new SVG icons and update Main component to include them
    - Added SVG icons for CSS3, TypeScript, Node.js, Tailwind CSS, REST API, GraphQL, and Vercel.
    - Updated Main.jsx to import and display the new icons in the skills grid.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,17 @@
 import "./Main.css";
 import { Typewriter } from "react-simple-typewriter";
 import jsIcon from "../../assets/icons/javascript-16-svgrepo-com.svg";
+import tsIcon from "../../assets/icons/typescript-16-svgrepo-com.svg";
 import reactIcon from "../../assets/icons/react.svg";
+import nodeIcon from "../../assets/icons/node-svgrepo-com.svg";
 import gitIcon from "../../assets/icons/github-svgrepo-com.svg";
 import htmlIcon from "../../assets/icons/html-fill-svgrepo-com.svg";
+import cssIcon from "../../assets/icons/css3-01-svgrepo-com (1).svg";
+import tailwindIcon from "../../assets/icons/tailwind-svgrepo-com.svg";
+import gcpIcon from "../../assets/icons/brand-google-cloud-platform-svgrepo-com (1).svg";
+import vercelIcon from "../../assets/icons/vercel-svgrepo-com.svg";
+import restApiIcon from "../../assets/icons/rest-api-svgrepo-com.svg";
+import graphqlIcon from "../../assets/icons/graphql-svgrepo-com.svg";
 
 function Main() {
   return (
@@ -44,9 +52,21 @@ function Main() {
         </section>
         <div className="main__skills-grid main__skills-icons-only">
           <img src={jsIcon} alt="JavaScript" title="JavaScript" />
+          <img src={tsIcon} alt="TypeScript" title="TypeScript" />
           <img src={reactIcon} alt="React.js" title="React.js" />
+          <img src={nodeIcon} alt="Node.js" title="Node.js" />
           <img src={gitIcon} alt="Git" title="Git" />
           <img src={htmlIcon} alt="HTML5" title="HTML5" />
+          <img src={cssIcon} alt="CSS3" title="CSS3" />
+          <img src={tailwindIcon} alt="Tailwind CSS" title="Tailwind CSS" />
+          <img src={restApiIcon} alt="REST API" title="REST API" />
+          <img src={graphqlIcon} alt="GraphQL" title="GraphQL" />
+          <img src={vercelIcon} alt="Vercel" title="Vercel" />
+          <img
+            src={gcpIcon}
+            alt="Google Cloud Platform"
+            title="Google Cloud Platform"
+          />
         </div>
       </div>
     </main>
